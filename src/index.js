d408360d22b73dd6628d3926e6f8d8e170a79855
Eric Wu <eric.dianhao.wu@gmail.com>
2022-02-05 22:15:55 -0800

    did work on project. There is now a function to detect the winner, and players take turns. However, still need to check if square is actually available

---
+++ src/index.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+// class Square extends React.Component {
+// 	// constructor(props){
+// 	// 	super(props)
+// 	// 	this.state = {
+// 	// 		value: null,
+// 	// 	}
+// 	// }
+
+
+// 	render() {
+// 		return (
+// 			<button className="square" 
+// 				onClick={()=>this.props.onClick()}>
+// 				{this.props.value}
+// 			</button>
+// 		);
+// 	}
+// }
+
+function Square(props) {
+	return (
+		<button className="square"
+			onClick={props.onClick}>
+			{props.value}
+		</button>
+	)
+}
+
+class Board extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			squares: Array(9).fill(null),
+			xIsNext: true
+		}
+	}
+
+	handleClick(i) {
+		//console.log("handled click!!!")
+		const squares = this.state.squares.slice()
+		squares[i] = this.state.xIsNext ? "X" : "O"
+		this.setState({ squares: squares, xIsNext: !this.state.xIsNext })
+	}
+
+	renderSquare(i) {
+		return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+	}
+
+	render() {
+
+		let status
+		const winner = calculateWinner(this.state.squares)
+		if(winner){
+			status = 'Winner: ' + winner
+		} else {
+			status = 'Next player: ' + (this.state.xIsNext ? "X" : "O")
+		}
+
+		return (
+			<div>
+				<div className="status">{status}</div>
+				<div className="board-row">
+					{this.renderSquare(0)}
+					{this.renderSquare(1)}
+					{this.renderSquare(2)}
+				</div>
+				<div className="board-row">
+					{this.renderSquare(3)}
+					{this.renderSquare(4)}
+					{this.renderSquare(5)}
+				</div>
+				<div className="board-row">
+					{this.renderSquare(6)}
+					{this.renderSquare(7)}
+					{this.renderSquare(8)}
+				</div>
+			</div>
+		);
+	}
+}
+
+class Game extends React.Component {
+	render() {
+		return (
+			<div className="game">
+				<div className="game-board">
+					<Board />
+				</div>
+				<div className="game-info">
+					<div>{/* status */}</div>
+					<ol>{/* TODO */}</ol>
+				</div>
+			</div>
+		);
+	}
+}
+
+function calculateWinner(squares) {
+	//function to find the winner of a specific squares arangement
+	let lines = [
+		[0, 1, 2], [3, 4, 5], [6, 7, 8],//horizontal lines
+		[0, 3, 6], [1, 4, 7], [2, 5, 8],//vertical lines
+		[0, 4, 8], [2, 4, 6]					//diagonals
+	]
+
+	for (let line of lines) {
+		if (squares[line[0]] && squares[line[0]] == squares[line[1]] && squares[line[0]] == squares[line[2]]) {
+			return squares[line[0]]
+		}
+	}
+	return null
+
+}
+
+
+// ========================================
+
+ReactDOM.render(
+	<Game />,
+	document.getElementById('root')
+);
